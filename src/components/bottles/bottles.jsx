5361cddd77435de5c6f7134c09bf9a674bac625b
coder-utshab <ghoshutshob470@gmail.com>
2026-07-19 01:45:34 +0530

    up

--- src/components/bottles/bottles.jsx
+++ src/components/bottles/bottles.jsx
@@ -1,46 +1,4 @@
 
-
-// import { useEffect, useState } from "react";
-// import Bottle from "../Bottle/Bottle";
-// // import Bottle from "../Bottle/Bottle";
-// import './bottles.css';
-// const Bottles = () => {
-
-//     const [bottles, setBottles] = useState([]);
-
-//     useEffect(() => {
-//         fetch('bottles.json')
-//             .then(res => res.json())
-//             .then(data => setBottles(data));
-//     }, []);
-
-//     return (
-//         <div>
-//             <h2>Bottles Here: {bottles.length}</h2>
-//           <div className="bottles-container">
-//     {
-//         bottles.map(bottle => (
-//             <Bottle
-//                 key={bottle.id}
-//                 bottle={bottle}
-//             />
-//         ))
-//     }
-// </div>
-            
-//         </div>
-//     );
-// };
-
-// export default Bottles;
-
-
-
-
-
-
-
-
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./bottles.css";
