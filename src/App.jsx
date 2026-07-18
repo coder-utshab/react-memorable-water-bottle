2ac380e886645a149802a74584e731c7f10b2acd
coder-utshab <ghoshutshob470@gmail.com>
2026-07-19 00:25:19 +0530

    up

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import './App.css'
+// import Heaader from './components/Header/Header'
+
+// function App() {
+
+
+//   return (
+//     <>
+//      <header></header>
+//      <Bottles></Bottles>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+import './App.css';
+import Header from './components/Header/Header';
+import Bottles from './components/Bottles/Bottles';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Bottles />
+    </>
+  );
+}
+
+export default App;
